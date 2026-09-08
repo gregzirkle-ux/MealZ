535c8cdae1f39d9d7ce3a302924615d331e28dd0
gregzirkle-ux <gregzirkle@gmail.com>
2026-09-08 17:37:25 -0400

    Update Supabase configuration in config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Mealz shared mode configuration.
 // Leave these blank to use local Demo Mode on one device.
 window.MEALZ_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://qcezhjociexuinglumuk.supabase.co',
+  supabasePublishableKey: 'sb_publishable_e7VTgQ0zFDX9w5sj3-o2hg_2CG6c-C6'
 };
 // Backward-compatible alias for the original prototype.
 window.WEEKNIGHT_CONFIG = window.MEALZ_CONFIG;
